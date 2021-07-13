c1606b2be4660ef65c9189f2fbe712ac3db7131b
luca-gugs <56174382+luca-gugs@users.noreply.github.com>
2021-07-13 00:24:27 -0400

    improvements

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      boxShadow: {
+        nav: '0 1px 1px rgba(0,0,0,0.12), 0 2px 2px rgba(0,0,0,0.12), 0 4px 4px rgba(0,0,0,0.12), 0 8px 8px rgba(0,0,0,0.12), 0 16px 16px rgba(0,0,0,0.12)',
+      },
       height: {
         'mobile-screen': '85vh',
       },
